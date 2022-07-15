89dea4db379614a836fc2e741c718ed0504a43d4
TheCodePrism <46435135+Arijit265@users.noreply.github.com>
2022-07-15 21:58:38 +0530

    ho

---
+++ js/home.js
@@ -0,0 +1,155 @@
+
+
+var ajax_form = true;
+
+$(document).ready(function () { 
+
+
+
+
+
+
+    var cbpAnimatedHeader = (function () {
+
+        var docElem = document.documentElement,
+            header = document.querySelector('.cbp-af-header'),
+            didScroll = false,
+            changeHeaderOn = 100;
+
+        function init() {
+            window.addEventListener('scroll', function (event) {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 0);
+                }
+            }, false);
+        }
+
+        function scrollPage() {
+            var sy = scrollY();
+            if (sy >= changeHeaderOn) {
+                classie.add(header, 'cbp-af-header-shrink');
+            } else {
+                classie.remove(header, 'cbp-af-header-shrink');
+            }
+            didScroll = false;
+        }
+
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }
+
+        init();
+
+    })();
+
+
+
+
+
+
+
+	
+
+$('.goto').click(function(e){
+ e.preventDefault();
+    $('html').scrollTo(this.hash,this.hash);
+   
+});
+
+
+
+
+
+    mediaCheck({
+        media: '(max-width: 768px)',
+        entry: function () {
+
+            $('.make-it-appear-top').waypoint(function (direction) {
+                $(this).css('opacity', '1');
+            }, {
+                offset: '200%'
+            });
+
+            $('.make-it-appear-left').waypoint(function (direction) {
+                $(this).css('opacity', '1');
+            }, {
+                offset: '200%'
+            });
+
+            $('.make-it-appear-right').waypoint(function (direction) {
+                $(this).css('opacity', '1');
+            }, {
+                offset: '200%'
+            });
+
+            $('.make-it-appear-bottom').waypoint(function (direction) {
+                $(this).css('opacity', '1');
+            }, {
+                offset: '200%'
+            });
+
+
+        },
+        exit: function () {
+
+
+
+
+            $('.make-it-appear-top').waypoint(function (direction) {
+                $(this).addClass('animated fadeInDown');
+            }, {
+                offset: '80%'
+            });
+
+            $('.make-it-appear-left').waypoint(function (direction) {
+                $(this).addClass('animated fadeInLeft');
+            }, {
+                offset: '80%'
+            });
+
+            $('.make-it-appear-right').waypoint(function (direction) {
+                $(this).addClass('animated fadeInRight');
+            }, {
+                offset: '80%'
+            });
+
+            $('.make-it-appear-bottom').waypoint(function (direction) {
+                $(this).addClass('animated fadeInUp');
+            }, {
+                offset: '80%'
+            });
+
+            $('.bounce').waypoint(function (direction) {
+                $(this).addClass('animated bounce');
+            }, {
+                offset: '70%'
+            });
+
+            $('.pulse').waypoint(function (direction) {
+                $(this).addClass('animated pulse');
+            }, {
+                offset: '50%'
+            });
+
+        }
+
+
+    }); 
+
+
+}); 
+
+
+
+
+
+$(window).load(function () {
+   
+    jQuery("#loading-animation").fadeOut();
+   
+    jQuery("#preloader").delay(600).fadeOut("slow");
+
+
+
+});
